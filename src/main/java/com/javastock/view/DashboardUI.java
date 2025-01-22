a8afffc399fa5cfd8e42f508d309532ba56b9453
RobPerdiguerra <perdiguerrarobinlouis@gmail.com>
2025-01-22 15:38:18 +0800

    Added panels to DashboardUI

--- src/main/java/com/javastock/view/DashboardUI.java
+++ src/main/java/com/javastock/view/DashboardUI.java
@@ -5,17 +5,36 @@ import main.java.com.javastock.viewmodel.DashboardVM;
 import javax.swing.*;
 import java.awt.*;
 
-public class DashboardUI {
-    private JPanel panel;
-    private DashboardVM viewModel;
+public class DashboardUI extends JPanel {
+    private DashboardVM dashboardVM;
+    private static final Font TITLE_FONT = new Font("Helvetica", Font.BOLD, 16);
 
     public DashboardUI(DashboardVM viewModel) {
-        this.viewModel = viewModel;
-        panel = new JPanel();
-        panel.add(new JLabel(viewModel.getDashboardData(), JLabel.CENTER));
-    }
+        this.dashboardVM = viewModel;
+        setLayout(new BorderLayout());
 
-    public JPanel getPanel() {
-        return panel;
+        //Add components to main Panel
+        add(createPadding(0,20), BorderLayout.NORTH);
+        add(createPadding(20,0), BorderLayout.EAST);
+        add(createPadding(20,0), BorderLayout.WEST);
+        add(createPadding(0,20), BorderLayout.SOUTH);
+        add(contentPanel(), BorderLayout.CENTER);
+    }
+    private JPanel createPadding(int width, int height) {
+        JPanel padding = new JPanel();
+        padding.setBackground(Color.LIGHT_GRAY);
+        padding.setPreferredSize(new Dimension(width, height));
+        return padding;
+    }
+    private JPanel contentPanel() {
+        //Content Panel
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        JLabel titleLabel = new JLabel("Manage Store");
+        titleLabel.setFont(TITLE_FONT);
+
+        contentPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        contentPanel.add(titleLabel);
+        return contentPanel;
     }
 }
